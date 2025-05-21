b3cd69aad1fedae877b36e8de78d6011e9c35de7
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-21 08:40:08 -0500

    feat: added io.mw

---
+++ apps/io.mw/app/layout.tsx
@@ -0,0 +1,57 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+
+export const metadata = {
+  // Define your metadata here
+  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+}
+
+// const banner = <Banner storageKey='some-key'>Nextra 4.0 is released 🎉</Banner>
+const navbar = (
+  <Navbar
+    logo={
+      <p>
+        <b>MDX</b>.org.AI
+      </p>
+    }
+    // ... Your additional navbar options
+  />
+)
+const footer = (
+  <Footer>
+    MIT {new Date().getFullYear()} © <a href='https://dotdo.ai'>.do AI</a>
+  </Footer>
+)
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang='en'
+      // Required to be set
+      dir='ltr'
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <Layout
+          // banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase='https://github.com/drivly/mdx/tree/main/apps/mdx.org.ai'
+          footer={footer}
+          // ... Your additional layout options
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  )
+}
