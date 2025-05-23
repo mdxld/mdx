6ae73c42a759602d4eb8081faef767086038034e
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-23 06:10:42 -0500

    tests: added tests

---
+++ tests/mdxdb.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest'
+import { db } from '@mdxdb/core'
+
+describe('mdxdb api', () => {
+  it('should list all of the .md and .mdx files', () => {
+    const list = db.list()
+    expect(list.length).toBeGreaterThan(3)
+  })
+  it('should be able to get all readme files', () => {
+    const readme = db.get('**/readme.md')
+    expect(readme).toBeDefined()
+  })
+})
