74fbd9ac7b2f390e108e2d21f037bb9b8f0b24e6
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-30 08:13:11 -0500

    feat: added workflow function

---
+++ packages/mdxai/src/functions/workflow.test.ts
@@ -0,0 +1,14 @@
+import { workflow } from './workflow'
+
+describe('workflow', () => {
+  it('should generate a workflow', async () => {
+    const result = await workflow`to flesh out a startup idea and validate it with the Disciplined Entrepreneurship framework`
+    console.log(result)
+  })
+
+  it('should generate a workflow', async () => {
+    const result = await workflow`to verify auto lender stipulations`
+    console.log(result)
+  })
+
+})
